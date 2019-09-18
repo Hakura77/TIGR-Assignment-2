d72f7427d2dc7da090590c6002cb4dfa24667e71
Hakura77 <36619964+hakura77@users.noreply.github.com>
2019-09-18 16:36:07 +1200

    implemented _build_command() method extracted method and intergrated it with the rest of the parse() method.
    Ran unit tests and program still passes

--- TigrParser.py
+++ TigrParser.py
@@ -57,49 +57,27 @@ class TigrParser(AbstractParser):
         else:
             raise SyntaxError(f"Command {command_string} on line {line_number} not recognized")
 
-
-
-
-
     def parse(self, raw_source):
         """Method to accept raw source code, parse to language commands, and then execute language commands"""
         source = self._prepare_source(raw_source)
         for line_number in range(0, len(source) - 1):
             match = self._trim_and_validate_line(source[line_number], line_number)
             if match:
-                groups = match[0]
-                command = groups[0].upper()
-                if groups[1]:
-                    command_data = int(round(float(groups[1])))
-                    """ Parser accepts decimals but silently rounds them in the background - all numbers passed are
-                    stored as integers"""
-                else:
-                    command_data = None
-
-                command_info = self.language_commands.get(command)
-                if command_info:
-                    args = []
-                    if len(command_info) > 1:
-                        args.append(*command_info[1])
-                    if command_data:
-                        args.append(command_data)
-
-                    # explodes the created args array into the function that is being called
-                    # if there is nothing in the array, nothing will be passed! Nice and fancy.
-                    try:
-                        self.drawer.__getattribute__(command_info[0])(*args)
-                    except AttributeError as e:
-                        raise SyntaxError(
-                            f'Command {command} Not recognized by drawer - Command reference mismatch detected')
-                    except Exception as e:  # intercept error thrown that wasn't caught and appending the line number
-                        # that caused it
-                        args = e.args
-                        if args:
-                            arg0 = args[0]
-                        else:
-                            arg0 = str()
-                        arg0 += f' at source line {line_number}'
-                        e.args = (arg0, *args[1:])
-                        raise
-                else:
-                    raise SyntaxError(f"Command {command} on line {line_number} not recognized")
+                command, arguments = self._build_command(match[0], line_number)
+                # explodes the created args array into the function that is being called
+                # if there is nothing in the array, nothing will be passed! Nice and fancy.
+                try:
+                    self.drawer.__getattribute__(command)(*arguments)
+                except AttributeError as e:
+                    raise SyntaxError(
+                        f'Command {command} Not recognized by drawer - Command reference mismatch detected')
+                except Exception as e:  # intercept error thrown that wasn't caught and appending the line number
+                    # that caused it
+                    args = e.args
+                    if args:
+                        arg0 = args[0]
+                    else:
+                        arg0 = str()
+                    arg0 += f' at source line {line_number}'
+                    e.args = (arg0, *args[1:])
+                    raise
