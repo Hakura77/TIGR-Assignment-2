81b3a1b5f8287c9db21fe7cf939b9038dc04bf90
Hakura77 <signin4stuff@gmail.com>
2019-09-25 18:15:01 +1200

    Created TIGrErrorManager and added the errors that the program can generate to its list of managed errors. Also replicated the error mutation functionality from inside the parser and the error logging catch from the reader

---
+++ TIGrErrorManager.py
@@ -0,0 +1,45 @@
+"""
+
+Created by Thomas Baines
+"""
+import sys
+
+
+class TIGrErrorManager:
+
+    def __init__(self):
+        self.exit_message = "TIGr encountered an error and had to exit"
+
+    def log_error_and_exit(self, error_to_log):
+        """Program standard behaviour for """
+        print(self.exit_message, file=sys.stderr)
+        print(error_to_log, file=sys.stderr)
+        exit(1)
+
+    def mutate_other_exceptions(self, e, line_number):
+        """Intercepts errors that are thrown and not explicitly handled - indicating their
+        line_number providence """
+        args = e.args
+        if args:
+            arg0 = args[0]
+        else:
+            arg0 = str
+        arg0 += f' at source line {line_number}'
+        e.args = (arg0, *args[1:])
+        return e
+
+    def drawer_language_missmatch_error(self, command_text):
+        """This error is thrown when the language database contains a command not supported by drawer"""
+        return SyntaxError(
+            f'Command {command_text} Not recognized by drawer - Command reference mismatch detected')
+
+    def unrecognized_command_error(self, command_string, line_number):
+        """This error is thrown when the command passed in does not exist within the language database"""
+        return SyntaxError(f"Command {command_string} on line {line_number} not recognized")
+
+    def invalid_line_syntax_error(self, line_number, line_value):
+        return SyntaxError(f"Command {line_value} on line {line_number} not recognized")
+
+
+    def invalid_direction_error(self, direction):
+        return ValueError(f"Direction given was {direction}, must be between 0 - 360")
